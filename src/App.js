79f544c50e091d8d962b2aca82accb09102e841b
Hugo <71639139+yuugodev@users.noreply.github.com>
2020-10-13 18:26:34 -0300

    002

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>
+        Emmet is not working, neither other features like automatic close tags
+      </p>
     </div>
   );
 }
